8792601c1f5b298036b442e699e72065c5e7c589
Don Wibier <don@anykey.nl>
2024-09-18 15:37:02 +0200

    Initial check-in

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,109 @@
+﻿using DevExpress.Xpo;
+using DX.Data;
+using DX.Utils;
+using DXMVCTestApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+using static DevExpress.Xpo.Helpers.AssociatedCollectionCriteriaHelper;
+
+namespace DXMVCTestApplication.Controllers
+{
+	public interface IXpoController
+	{
+		IDataLayer DataLayer { get; }
+	}
+	public class BaseController : Controller, IXpoController
+	{
+		public BaseController()
+		{
+			dataLayer = XpoHelper.GetDataLayer();
+		}
+
+		IDataLayer dataLayer;
+		public IDataLayer DataLayer => dataLayer;
+
+	}
+
+	public class BaseController<TMainStore, TKey, TModel> : BaseController
+		where TMainStore: DX.Data.IQueryableDataStore<TKey, TModel>
+		where TKey : IEquatable<TKey>
+		where TModel : class, new()	
+	{
+		public TMainStore MainStore { get; }
+		public virtual string PartialViewName { get; } = "GridViewPartialView";
+		public BaseController() : base()
+		{			
+			MainStore = (TMainStore)Activator.CreateInstance(typeof(TMainStore), DataLayer);
+		}
+
+		public async virtual Task<ActionResult> Index()
+		{
+			var data = await MainStore.Query().ToListAsync();
+			return View(data);
+		}
+
+		public async virtual Task<ActionResult> DXControlPartialView()
+		{
+			var data = await MainStore.Query().ToListAsync();
+			return PartialView(PartialViewName, data);
+		}
+
+
+		protected IDataResult<TKey, TModel> HandleValidation(IDataResult<TKey, TModel> dataResult, TModel model)
+		{
+			if (!dataResult.Success)
+			{
+				ViewData["EditError"] = string.Join("\\n", dataResult.Exception.Errors);
+				ViewData["EditableItem"] = model;
+				foreach (var error in dataResult.Exception.Errors)
+					ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+			}
+			return dataResult;				
+		}
+
+		//[HttpPost, ValidateInput(true)]
+		public async virtual Task<ActionResult> AddNew(TModel item)
+		{
+			if (ModelState.IsValid)
+			{
+				var result = HandleValidation(await MainStore.CreateAsync(item), item);
+			}
+			else
+			{
+				ViewData["EditError"] = "Please, correct all errors.";
+				ViewData["EditableItem"] = item;
+			}
+			return await DXControlPartialView();
+		}
+
+		//[HttpPost, ValidateInput(true)]
+		public async virtual Task<ActionResult> Update(TModel item)
+		{
+			if (ModelState.IsValid)
+			{
+				var result = HandleValidation(await MainStore.UpdateAsync(item), item);
+			}
+			else
+			{
+				ViewData["EditError"] = "Please, correct all errors.";
+				ViewData["EditableItem"] = item;
+			}
+			return await DXControlPartialView();
+		}
+
+		//[HttpPost, ValidateInput(false)]
+		public async  virtual Task<ActionResult> Delete(TKey oid)
+		{
+			await MainStore.DeleteAsync(oid);
+			return await DXControlPartialView();
+		}
+
+
+
+	}
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using DevExpress.Xpo;
+using DXMVCTestApplication.Models;
+using DXMVCTestApplication.Models.XPO;
+
+
+namespace DXMVCTestApplication.Controllers
+{
+    public class HomeController : BaseController<PatientStore, int, Patient>
+    {
+		
+		public HomeController() : base()
+		{
+		}
+
+		[HttpPost, ValidateInput(true)]
+		public async override Task<ActionResult> AddNew(Patient item)
+		{
+			return await base.AddNew(item);
+		}
+
+
+		[HttpPost, ValidateInput(true)]
+		public async override Task<ActionResult> Update(Patient item)
+		{
+			return await base.Update(item);
+		}
+
+
+		[HttpPost, ValidateInput(false)]
+		public async override Task<ActionResult> Delete(int oid)
+		{
+			return await base.Delete(oid);
+		}
+	}
+}
